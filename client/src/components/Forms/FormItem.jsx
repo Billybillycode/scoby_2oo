f36ae37cd29d928b1ac2a4967661dfa5541039c5
Gwerborc'h Mahéo <gwerborch.maheo.com>
2021-01-12 17:12:52 +0100

    Form in progress

--- client/src/components/Forms/FormItem.jsx
+++ client/src/components/Forms/FormItem.jsx
@@ -1,59 +1,89 @@
 import React, { Component } from "react";
 import LocationAutoComplete from "../LocationAutoComplete";
 import "../../styles/form.css";
-
+import apiHandler from "../../api/apiHandler";
 class ItemForm extends Component {
-  state = {};
-
-  handleChange(event) {
-    console.log("Wax On Wax Off");
-    this.setState({});
-  }
-
+  // static contextType = UserContext;
+  state = {
+    name: "",
+    category: "",
+    quantity: 0,
+    address: "",
+    description: "",
+  };
+  formRef = React.createRef();
+  handleChange = (event) => {
+    // console.log("Wax On Wax Off");
+    const key = event.target.name;
+    const value =
+      event.target.type === "file"
+        ? event.target.files[0]
+        : event.target.type === "checkbox"
+        ? event.target.checked
+        : event.target.value;
+    console.log("key :", key);
+    console.log("value :", value);
+    this.setState({ [key]: value });
+  };
   handleSubmit = (event) => {
     event.preventDefault();
-    console.log("Wax On Wax Off");
-
+    // console.log("Wax On Wax Off");
+    // this.context.item({
+    //   name: this.state.name,
+    //   category: this.state.category,
+    //   quantity: this.state.quantity,
+    //   address: this.state.address,
+    //   description: this.state.description,
+    apiHandler
+      .addItem(this.state)
+      .then((data) => {
+        this.props.history.push("/");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
     // In order to send back the data to the client, since there is an input type file you have to send the
     // data as formdata.
     // The object that you'll be sending will maybe be a nested object, in order to handle nested objects in our form data
     // Check out the stackoverflow solution below : )
-
     // Nested object into formData by user Vladimir "Vladi vlad" Novopashin @stackoverflow : ) => https://stackoverflow.com/a/42483509
   };
-
   handlePlace = (place) => {
     // This handle is passed as a callback to the autocomplete component.
     // Take a look at the data and see what you can get from it.
     // Look at the item model to know what you should retrieve and set as state.
     console.log(place);
   };
-
   render() {
+    console.log("MY CURRENT STATE :", this.state);
     return (
       <div className="ItemForm-container">
-        <form className="form">
+        <form className="form" onSubmit={this.handleSubmit}>
           <h2 className="title">Add Item</h2>
-
           <div className="form-group">
             <label className="label" htmlFor="name">
               Name
             </label>
             <input
               id="name"
+              name="name"
               className="input"
               type="text"
-              onChange={this.handleChange}
+              value={this.state.name}
+              onChange={(event) => this.handleChange(event)}
               placeholder="What are you giving away ?"
             />
           </div>
-
           <div className="form-group">
             <label className="label" htmlFor="category">
               Category
             </label>
-
-            <select id="category" defaultValue="-1">
+            <select
+              id="category"
+              defaultValue="-1"
+              name="category"
+              onChange={(event) => this.handleChange(event)}
+            >
               <option value="-1" disabled>
                 Select a category
               </option>
@@ -63,41 +93,44 @@ class ItemForm extends Component {
               <option value="Kefir">Kefir</option>
             </select>
           </div>
-
           <div className="form-group">
             <label className="label" htmlFor="quantity">
               Quantity
             </label>
-            <input className="input" id="quantity" type="number" />
+            <input
+              className="input"
+              id="quantity"
+              type="number"
+              name="quantity"
+              onChange={(event) => this.handleChange(event)}
+            />
           </div>
-
           <div className="form-group">
             <label className="label" htmlFor="location">
               Address
             </label>
             <LocationAutoComplete onSelect={this.handlePlace} />
           </div>
-
           <div className="form-group">
             <label className="label" htmlFor="description">
               Description
             </label>
             <textarea
               id="description"
+              name="description"
               className="text-area"
               placeholder="Tell us something about this item"
+              onChange={this.handleChange}
+              value={this.state.description}
             ></textarea>
           </div>
-
           <div className="form-group">
             <label className="custom-upload label" htmlFor="image">
               Upload image
             </label>
             <input className="input" id="image" type="file" />
           </div>
-
           <h2>Contact information</h2>
-
           <div className="form-group">
             <label className="label" htmlFor="contact">
               How do you want to be reached?
@@ -109,18 +142,15 @@ class ItemForm extends Component {
             <input type="radio" />
             contact phone number
           </div>
-
           <p className="message">
             <img src="/media/info.svg" alt="info" />
             Want to be contacted by phone? Add your phone number in your
             personal page.
           </p>
-
           <button className="btn-submit">Add Item</button>
         </form>
       </div>
     );
   }
 }
-
 export default ItemForm;
